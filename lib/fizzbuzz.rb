0d1306f59e1081a17960ab3e4ea98ec6869eb7b7
nikklein <objcoding@gmail.com>
2016-06-28 11:36:51 +0100

    Finish with 'Fizzbuzz' and rspec

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,18 @@
 def fizzbuzz number
-'fizz'
+if number % 3 == 0 and number % 5 == 0
+  'fizzbuzz'
+elsif number % 3 == 0
+    #if number % 5 == 0
+      #'fizzbuzz'
+    #else
+      'fizz'
+  #  end
+  elsif number % 5 == 0
+    #if number % 3 == 0
+  #    'fizzbuzz'
+  #  elseif
+      'buzz'
+  else
+  number
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,16 @@
 require "fizzbuzz"
 
 describe 'fizzbuzz' do
-it 'returns "fizz" when passed 3' do
-expect(fizzbuzz(3)).to eq 'fizz'
-end
+  it 'returns "fizz" when passed a number that is a multiple of 3 ' do
+    expect(fizzbuzz(6)).to eq 'fizz'
+  end
+  it "returns 'buzz' when passed a number that is a multiple of 5" do
+    expect(fizzbuzz(20)).to eq 'buzz'
+  end
+  it "returns 'fizzbuzz' when passed a number that is a multiple of both 3 and 5" do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it "returns the given number in all other cases" do
+    expect(fizzbuzz(19)).to eq 19
+  end
 end
