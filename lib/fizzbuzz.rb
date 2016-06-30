000d76651593d7a5ce888ba3797809ac80acec7a
nikklein <objcoding@gmail.com>
2016-06-30 12:24:18 +0100

    Add a Fizzbuzz method into a Fixnum class

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,13 @@
 class Fixnum
   def fizzbuzz
-    if self == 3
+    if self % 3 == 0 && self % 5 == 0
+    'fizzbuzz'
+    elsif self % 3 == 0
       'fizz'
-    elsif self == 5
+    elsif self % 5 == 0
       'buzz'
+    else
+      self
     end
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,10 +1,16 @@
 require "fizzbuzz"
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" for the number 3' do
-    expect(3.fizzbuzz).to eq 'fizz'
+  it 'returns "fizz" for multiples of 3' do
+    expect(12.fizzbuzz).to eq 'fizz'
   end
-  it 'returns "buzz" for the number 5' do
-    expect(5.fizzbuzz).to eq 'buzz'
+  it 'returns "buzz" for the multiples of 5' do
+    expect(20.fizzbuzz).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" if number is a multiple of 3 and 5' do
+    expect(15.fizzbuzz).to eq 'fizzbuzz'
+  end
+  it 'returns the number for all other given numbers' do
+    expect(34.fizzbuzz).to eq 34
   end
 end
